ed1776a1f8a424a33e84401451f20abc436e17a2
samcolson4 <samcolson4@gmail.com>
2020-08-27 11:32:15 +0100

    Update test to include test for '3' and '15' outputting fizz and fizzbuzz respectively

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "buzz" for the number 5' do
     expect(5.fizzbuzz).to eq "buzz"
   end
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq "fizz"
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
+  end
 end
