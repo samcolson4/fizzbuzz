faa202bb470cf6b812b7ec0cdc54ad51d634c0b4
samcolson4 <samcolson4@gmail.com>
2020-08-27 11:42:33 +0100

    Update test to add tests for multiples

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,13 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" for the number 15' do
     expect(15.fizzbuzz).to eq "fizzbuzz"
   end
+  it 'returns "buzz" for the number 50' do
+    expect(50.fizzbuzz).to eq "buzz"
+  end
+  it 'returns "fizz" for the number 66' do
+    expect(66.fizzbuzz).to eq "fizz"
+  end
+  it 'returns "fizzbuzz" for the number 45' do
+    expect(45.fizzbuzz).to eq "fizzbuzz"
+  end
 end
